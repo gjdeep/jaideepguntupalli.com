08d04cdb5faefeb98d4e2fccf516b8237e6965a8
Jaideep <guntupallijaideep@gmail.com>
2022-08-07 21:41:41 +0530

    Update: Added sanity dependancy plugins

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 module.exports = {
     siteMetadata: {
         title: `Jaideep Guntupalli`,
@@ -5,12 +7,21 @@ module.exports = {
     },
     plugins: [
         "gatsby-plugin-postcss",
+        "gatsby-plugin-image",
         "gatsby-plugin-react-helmet",
         {
             resolve: `gatsby-plugin-manifest`,
             options: {
                 name: `Jaideep Guntupalli`,
                 icon: "src/images/favicon.png",
+            },
+        },
+        {
+            resolve: `gatsby-source-sanity`,
+            options: {
+                projectId: `yr7gi15i`,
+                dataset: `blog`,
+                token: process.env.SANITY_TOKEN,
             },
         },
     ],

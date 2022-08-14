27948eb0f247e35240f8c5f9a6e54d602b5cde7f
Jaideep <guntupallijaideep@gmail.com>
2022-08-14 13:31:52 +0530

    Feat: Custom 404 Page

--- web/src/pages/404.jsx
+++ web/src/pages/404.jsx
@@ -1,54 +1,39 @@
-import * as React from "react"
-import { Link } from "gatsby"
-
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+import * as React from "react";
+import Btn from "../components/common/Btn";
+import Layout from "../layout/Layout";
+import err from "../images/404.svg";
 
 // markup
 const NotFoundPage = () => {
-  return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
-}
+    return (
+        <Layout pageTitle="Blog | Jaideep Guntupalli">
+            <section className="pt-20 flex flex-col-reverse items-center md:flex-row md:gap-20">
+                <section className="pt-20 flex flex-col md:gap-8 items-start">
+                    <h1 className="text-9xl lg:text-[15rem] leading-none">
+                        404
+                    </h1>
+                    <div>
+                        <p className="text-4xl lg:text-6xl leading-none">
+                            Something went
+                        </p>
+                        <p className="text-6xl lg:text-8xl font-bold py-10">
+                            WRONG!
+                        </p>
+                    </div>
+                    <Btn
+                        text="Go back to home"
+                        addr="/"
+                        extclass="bg-cus-orange text-white hover:bg-cus-orange-dark"
+                    />
+                </section>
+                <img
+                    src={err}
+                    alt="404 illustration"
+                    className="sm:w-1/2 xl:w-[60%]"
+                />
+            </section>
+        </Layout>
+    );
+};
 
-export default NotFoundPage
+export default NotFoundPage;

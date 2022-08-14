5c3212638f9ee2d9a1022d3558bee758bfa70d2e
Jaideep <guntupallijaideep@gmail.com>
2022-08-14 13:34:27 +0530

    Update: Added graphql query to allBlog component

--- web/src/sections/blog/AllBlogs.jsx
+++ web/src/sections/blog/AllBlogs.jsx
@@ -1,10 +1,57 @@
 import * as React from "react";
+import { useStaticQuery, graphql } from "gatsby";
 import { BlogPreview } from "../../components/Blog/BlogPreview";
+import BlogGrid from "../../components/Blog/BlogGrid";
 import SectHeading from "../../components/common/SectHeading";
-import code from "../../images/blog/code.png";
-import jaideep from "../../images/blog/jaideep.jpg";
 
 const AllBlogs = () => {
+    const data = useStaticQuery(graphql`
+        query {
+            allSanityPost(sort: { fields: postedDate, order: DESC }) {
+                nodes {
+                    _id
+                    slug {
+                        current
+                    }
+                    title
+                    postedDate(formatString: "MMM DD, YYYY")
+                    coverImg {
+                        asset {
+                            resize(width: 1200, format: WEBP) {
+                                src
+                            }
+                        }
+                        alt
+                    }
+                    author {
+                        fName
+                        lName
+                        photo {
+                            alt
+                            asset {
+                                url
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    `);
+    const posts = data.allSanityPost.nodes.map((post) => {
+        console.log(post.coverImg);
+        return (
+            <BlogPreview
+                key={post._id}
+                img={post.coverImg.asset.resize.src}
+                imgAlt={post.coverImg.alt}
+                title={post.title}
+                slug={post.slug.current}
+                author={post.author.fName + " " + post.author.lName}
+                authorImg={post.author.photo.asset.url}
+                date={post.postedDate}
+            />
+        );
+    });
     return (
         <section className="pt-40">
             <SectHeading
@@ -13,57 +60,7 @@ const AllBlogs = () => {
                 linktext="View More"
                 addr="#"
             />
-            <section className="grid md:grid-cols-2 md:w-11/12 mx-auto">
-                <BlogPreview
-                    img={code}
-                    title="Hover Animation of a Button using only CSS"
-                    author="Jaideep Guntupalli"
-                    authorImg={jaideep}
-                    date="Apr 17, 2022"
-                />
-                <BlogPreview
-                    img={code}
-                    title="Hover Animation of a Button using only CSS"
-                    author="Jaideep Guntupalli"
-                    authorImg={jaideep}
-                    date="Apr 17, 2022"
-                />
-                <BlogPreview
-                    img={code}
-                    title="Hover Animation of a Button using only CSS"
-                    author="Jaideep Guntupalli"
-                    authorImg={jaideep}
-                    date="Apr 17, 2022"
-                />
-                <BlogPreview
-                    img={code}
-                    title="Hover Animation of a Button using only CSS"
-                    author="Jaideep Guntupalli"
-                    authorImg={jaideep}
-                    date="Apr 17, 2022"
-                />
-                <BlogPreview
-                    img={code}
-                    title="Hover Animation of a Button using only CSS"
-                    author="Jaideep Guntupalli"
-                    authorImg={jaideep}
-                    date="Apr 17, 2022"
-                />
-                <BlogPreview
-                    img={code}
-                    title="Hover Animation of a Button using only CSS"
-                    author="Jaideep Guntupalli"
-                    authorImg={jaideep}
-                    date="Apr 17, 2022"
-                />
-                <BlogPreview
-                    img={code}
-                    title="Hover Animation of a Button using only CSS"
-                    author="Jaideep Guntupalli"
-                    authorImg={jaideep}
-                    date="Apr 17, 2022"
-                />
-            </section>
+            <BlogGrid>{posts}</BlogGrid>
         </section>
     );
 };

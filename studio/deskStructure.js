358bc12ff6e4be15e90335f28b39c3ee0f8a1287
Jaideep <guntupallijaideep@gmail.com>
2022-08-14 09:19:55 +0530

    Update: Customized Sanity Studio

---
+++ studio/deskStructure.js
@@ -0,0 +1,29 @@
+import S from "@sanity/desk-tool/structure-builder";
+
+export default () =>
+    S.list()
+        .title("Base")
+        .items([
+            // Make a singleton of the document with ID “siteSettings”
+            S.listItem()
+                .title("Global Settings")
+                .child(
+                    S.document()
+                        .schemaType("siteSettings")
+                        .documentId("siteSettings")
+                ),
+            // Add a visual divider
+            S.divider(),
+            // Add the rest of the document types, but filter out the siteSettings type defined above
+            ...S.documentTypeListItems().filter(
+                (item) => item.getId() === "page" || item.getId() === "social"
+            ),
+            S.divider(),
+            ...S.documentTypeListItems().filter(
+                (item) =>
+                    item.getId() === "post" ||
+                    item.getId() === "tag" ||
+                    item.getId() === "author"
+            ),
+            S.divider(),
+        ]);
